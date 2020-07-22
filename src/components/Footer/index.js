8c512f6f07e4fa5216567e5bd525b9ef8201abf4
Lucas Miranda <lucas-m@outlook.com>
2020-07-22 16:12:55 -0300

    feat: footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,49 @@
 import React from 'react'
-import { Root } from './styles'
+import { Root, Credits } from './styles'
 
 const Footer = (props) => {
   return (
     <Root>
-      Code by: <a>Lucas Miranda</a>
-      Idea & design by: <a>Lorena</a>
-      Disclaimer: This project is a challenge ... . Check this and another
-      challenges on DevChallenge.com.br
+      <Credits>
+        <small>
+          Code by:{' '}
+          <a
+            href="https://lucas-lm.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lucas Miranda
+          </a>
+        </small>
+        <small>
+          Idea & design by:{' '}
+          <a
+            href="https://github.com/Lorenalgm"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lorena Montes
+          </a>
+        </small>
+      </Credits>
+      <small>
+        Disclaimer: This project is a front-end challenge available on{' '}
+        <a
+          href="https://devchallenge.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DevChallenge
+        </a>
+        . Check this and another challenges on{' '}
+        <a
+          href="https://devchallenge.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          devchallenge.com.br
+        </a>
+      </small>
     </Root>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,6 +2,66 @@ import styled from 'styled-components'
 
 export const Root = styled.footer`
   width: 100%;
-  background-color: #333;
+  background-color: ${(props) => props.theme.colors.primary + '30'};
+  color: ${(props) => props.theme.colors.primary};
+  padding: 4px 16px;
   grid-area: footer;
+
+  small {
+    display: block;
+    text-align: center;
+    font-size: 0.8em;
+
+    a {
+      color: ${(props) => props.theme.colors.primary};
+    }
+  }
+`
+
+export const Credits = styled.div`
+  color: ${(props) => props.theme.colors.primary};
+  display: flex;
+  justify-content: space-around;
+  width: 90%;
+  max-width: 600px;
+  margin: 8px auto;
+
+  small {
+    display: block;
+    font-size: 1em;
+    font-weight: 400;
+    margin: 4px;
+    line-height: 1;
+
+    a {
+      font-weight: 500;
+      display: inline-block;
+      text-decoration: none;
+      color: ${(props) => props.theme.colors.primary};
+
+      &::after {
+        content: '';
+        display: block;
+        width: 16px;
+        margin-left: -4px;
+        height: 2px;
+        background-color: orangered;
+        transition: 200ms;
+      }
+
+      &:hover::after {
+        margin-left: 0;
+        background-color: ${(props) => props.theme.colors.primary};
+        width: 100%;
+      }
+
+      &:active {
+        color: darkred;
+      }
+
+      &:active::after {
+        background-color: darkred;
+      }
+    }
+  }
 `
