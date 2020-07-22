32c8b21e29c4f0d80f0143f395429aeaf4aece76
Lucas Miranda <lucas-m@outlook.com>
2020-07-22 12:23:53 -0300

    build: next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  exportTrailingSlash: true,
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
     return {
+      '/home': { page: '/' },
+      '/home/': { page: '/' },
       '/advice/': { page: '/advice' },
     }
   },
