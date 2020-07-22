2d9595138bf1beccf5ea903bd229aaa900bae4b7
Lucas Miranda <lucas-m@outlook.com>
2020-07-22 01:32:41 -0300

    feat: goats names

--- src/pages/advice/index.js
+++ src/pages/advice/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import axios from 'axios'
 import Logo from '../../components/Logo'
 import Root, { Card } from '../../components/Layouts/Advice'
+
+const prefixes = [
+  'Mr.',
+  'Miss',
+  'Lazy',
+  'Happy',
+  'Rich',
+  'Screaming',
+  'Another',
+  'Strange',
+]
 
 export async function getServerSideProps(context) {
   const { data } = await axios.get('https://api.adviceslip.com/advice')
@@ -25,7 +35,7 @@ export default function Advise(props) {
         <Card.Quote>
           <q>{props.advice}</q>
           <br />
-          <small>&mdash; Goat</small>
+          <small>&mdash; {prefixes[props.id % 8]} Goat</small>
         </Card.Quote>
         <Card.Image src="https://placegoat.com/400/400" alt="goat" />
       </Card>
